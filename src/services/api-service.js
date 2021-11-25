a11f72a68ae022636e8c3aa45e11ac65b443721a
MaksVibe <parunovmaksym@gmail.com>
2021-11-25 02:57:14 +0200

    11 Task

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,18 +1,21 @@
 const API_KEY = "24483741-f975205992ca850031cfb75db";
 const GALLERY_URL = `https://pixabay.com/api/?key=${API_KEY}`;
 
-const fetchGallery = (photos) => {
-  const queryParams = new URLSearchParams({
-    access_key: API_KEY,
-    query: photos,
-  });
+let page = 1;
+const incrementPage = () => (page += 1);
+const resetPage = () => (page = 1);
 
-  return fetch(`${WEATHER_URL}?${queryParams}`)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.error) {
-        return Promise.reject(new Error(data.error.info));
+const fetchGallery = (name) => {
+  return fetch(
+    `${GALLERY_URL}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(new Error(error));
       }
-      return data;
-    });
+      return response.json();
+    })
+    .then((data) => data);
 };
+
+export { fetchGallery, incrementPage, resetPage, page };
